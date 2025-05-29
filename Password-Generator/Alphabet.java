ad4d09bf6e0805316d2e5334ebca4edb972d6721
Varun-111 <110555164+Varun-111@users.noreply.github.com>
2025-05-29 12:10:14 +0530

    Create Alphabet.java

---
+++ Password-Generator/Alphabet.java
@@ -0,0 +1,25 @@
+public class Alphabet {
+	public static final String Uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static final String Lowercase_Letters = "abcdefghijklmnopqrstuvwxyz";
+	public static final String Numbers = "1234567890";
+	public static final String Symbols = "!@#$%^&*()-=_+\\/~?";
+	
+	private final StringBuilder pool;
+	
+	public Alphabet(boolean uppercaseIncluded, boolean lowercaseIncluded, boolean numbersIncluded, boolean specialCharactersIncluded) {
+		pool = new StringBuilder();
+		if(uppercaseIncluded) pool.append(Uppercase_letters);
+		
+		if(lowercaseIncluded) pool.append(Lowercase_Letters);
+		
+		if(numbersIncluded) pool.append(Numbers);
+		
+		if(specialCharactersIncluded) pool.append(Symbols);
+		
+	}
+	
+	
+	public String getAlphabet() {
+		return pool.toString();
+	}
+}
