c260a9cdc5235a471fcb048e04361d56ffb3c9c7
Varun-111 <110555164+Varun-111@users.noreply.github.com>
2025-05-29 12:02:19 +0530

    Update Main.java

--- Sample-Quiz-Game/Main.java
+++ Sample-Quiz-Game/Main.java
@@ -1,1 +1,8 @@
-
+#Main Class
+public class Main{
+    public static void main(String[] args) {
+        QuestionService questionService = new QuestionService();
+        questionService.playQuiz();
+        questionService.printScore();
+    }
+}
